78b20e2c7c6ed530a82583da7b3924da2736bcfd
jcpwfloi <jcpwfloi@gmail.com>
2020-01-09 23:37:27 -0500

    ex1

--- ex1.c
+++ ex1.c
@@ -17,9 +17,9 @@ main(int argc, char **argv)
   // the value of x by 1 using in-line assembly
   //
 
-  asm ("inc %1"
+  asm ("inc %0"
       : "=r" (x)
-      : "r" (x));
+      : "0" (x));
 
   printf("Hello x = %d after increment\n", x);
 
