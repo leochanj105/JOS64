d1365ea5fc9af5a5c234b02a828890c0757e0068
Jingyuan Chen <leochanj@walter.cs.unc.edu>
2020-03-25 16:01:42 -0400

    sysenterhalf

--- kern/init.c
+++ kern/init.c
@@ -79,9 +79,7 @@ i386_init(void)
 	ENV_CREATE(user_divzero, ENV_TYPE_USER);
 =======
 	ENV_CREATE(user_hello, ENV_TYPE_USER);
->>>>>>> lab3
 	//ENV_CREATE(user_buggyhello, ENV_TYPE_USER);
->>>>>>> lab3*/
 #endif // TEST*
 
 	// Schedule and run the first user environment!
